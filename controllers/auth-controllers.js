b5c45663d323f37e5c7bd798d1f11c8d922b100f
Vera D <veradanicode@gmail.com>
2025-08-27 16:02:51 +0100

    add auth register controller

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,9 +1,48 @@
-
+const User =require('../models/User')
+const bcrypt =require('bcryptjs')
 
 // register controllers
  const registerUser = async(req,res)=>{
     try {
-        
+        //extract information from our request body
+        const {username,email,password,role}=req.body;
+
+        //check if the user already exists in datatbase
+        const checkExistingUser =await User.findOne({$or:[{username},{email}]})
+        if (checkExistingUser) {
+            return res.status(400).json({
+                success:false,
+                message:"User already exists with this email/username!Try with different username/email"
+            })
+        }
+
+        //hash user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword =await bcrypt.hash(password,salt)
+
+        //create new user and save
+        const newUser = new User({
+            username,
+            email,
+            password:hashedPassword,
+            role:role ||"user"
+        })
+
+        await newUser.save();
+
+        if (newUser) {
+            res.status(201).json({
+                success:true,
+                message:"User created successfully"
+            })
+        }else{
+            res.status(400).json({
+                success:false,
+                message:"Unable to create user!please try again"
+            })
+        }
+
+
     } catch (err) {
         console.log(err);
         res.status(500).json({
