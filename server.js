b078a61c07bcaa7a94d2103f0aba9f984c0145b8
Vera D <veradanicode@gmail.com>
2025-08-27 13:03:12 +0100

    add auth routes and express middlewares

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
 const express =require('express')
 const connectToDB = require('./database/db')
+const authRoutes =require('./routes/auth-routes')
 
 //connect to DB
 connectToDB()
 
 const app = express();
 const PORT= 3000
+
+//middlewares
+app.use(express.json())
+
+//routes
+app.use('/api/auth',authRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Server is now running at port ${PORT}`);
