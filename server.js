6ad74caec1f8dddf0aeb319dc2ed880e0f1486d8
Vera D <veradanicode@gmail.com>
2025-08-28 18:33:24 +0100

    added home routes fixed PORT

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ require('dotenv').config()
 const express =require('express')
 const connectToDB = require('./database/db')
 const authRoutes =require('./routes/auth-routes')
+const homeRoutes =require('./routes/home-routes')
+
 
 //connect to DB
 connectToDB()
 
 const app = express();
-const PORT= 3000
+const PORT= process.env.PORT ||3000
 
 //middlewares
 app.use(express.json())
 
 //routes
 app.use('/api/auth',authRoutes)
+app.use('/api/home',homeRoutes)
+
 
 app.listen(PORT,()=>{
     console.log(`Server is now running at port ${PORT}`);
