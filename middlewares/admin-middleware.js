2314784b611e62ee1dfba1b450104f06132f30d9
Vera D <veradanicode@gmail.com>
2025-08-29 17:24:41 +0100

    rename middleware

---
+++ middlewares/admin-middleware.js
@@ -0,0 +1,14 @@
+
+
+const isAdmin =(req,res,next)=>{
+
+    if (req.userInfo.role != 'admin') {
+        return res.status(403).json({
+            success:false,
+            message:"Access denied! Admin rights required."
+        })
+    }
+    next();
+}
+
+module.exports =isAdmin;
