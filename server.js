452461c1774825e0ea5d6679bcfee7bb34292e92
Vera D <veradanicode@gmail.com>
2025-08-29 16:49:53 +0100

    imported and use admin and home route in server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express =require('express')
 const connectToDB = require('./database/db')
 const authRoutes =require('./routes/auth-routes')
 const homeRoutes =require('./routes/home-routes')
+const adminRoutes =require('./routes/admin-routes')
+
 
 
 //connect to DB
@@ -17,6 +19,8 @@ app.use(express.json())
 //routes
 app.use('/api/auth',authRoutes)
 app.use('/api/home',homeRoutes)
+app.use('/api/admin',adminRoutes)
+
 
 
 app.listen(PORT,()=>{
