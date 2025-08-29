bc24e246b992211ea1c744a767a405f279913ded
Vera D <veradanicode@gmail.com>
2025-08-29 16:46:54 +0100

    added auth logic

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,9 +1,34 @@
+require('dotenv').config()
+const jwt =require('jsonwebtoken')
 
 const authMiddleware = (req,res,next)=>{
     const authHeader = req.headers["authorization"];
     console.log(authHeader);
+
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({
+            success:false,
+            message:"Access denied!Please login to continue"
+        })
+    }
     
-    next()
+    try {
+        //decode token
+        const decodedTokenInfo =jwt.verify(token,process.env.JWT_SECRET_KEY);
+        console.log(decodedTokenInfo);
+
+        req.userInfo =  decodedTokenInfo;
+        next();
+        
+    } catch (error) {
+       return res.status(500).json({
+            success:false,
+            message:"Access denied!Please login to continue"
+        }) 
+    }
+
 }
 
 
