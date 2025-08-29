a25164389d359c09a656f31b505fd8ce1bc60826
Vera D <veradanicode@gmail.com>
2025-08-29 17:26:44 +0100

    added amin auth to verify role of user in route

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const authMiddleware =require('../middlewares/auth-middleware')
+const adminMiddleware =require('../middlewares/admin-middleware')
 
 
-router.get('/welcome',authMiddleware,(req,res)=>{
+
+router.get('/welcome',authMiddleware,adminMiddleware,(req,res)=>{
     res.json({
         message:"Welcome to the admin page"
     })
