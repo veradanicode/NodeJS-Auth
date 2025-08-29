56c26b6231ef41edac28ddb64018709c6c59c75d
Vera D <veradanicode@gmail.com>
2025-08-29 17:05:02 +0100

    added auth middleware in router

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const authMiddleware =require('../middlewares/auth-middleware')
 
 
-router.get('/welcome',(req,res)=>{
+router.get('/welcome',authMiddleware,(req,res)=>{
     res.json({
         message:"Welcome to the admin page"
     })
