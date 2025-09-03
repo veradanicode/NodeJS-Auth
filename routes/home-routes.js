8a8c252037cf3d5e37e1cbbf9936a367d635a617
Vera D <veradanicode@gmail.com>
2025-09-03 23:23:36 +0100

    update commit

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -6,7 +6,7 @@ const router =express.Router();
 router.get('/welcome',authMiddleware,(req,res)=>{
     const {username,userId,role} =req.userInfo;
     res.json({
-        message:`Welcome to the homePage ${username}`,
+        message:`Welcome to the homePage, ${username}`,
         user:{
             _id:userId,
             username,
