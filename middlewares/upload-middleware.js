e555f33d23887a4499c21d46f1416f17db2b0e03
Vera D <veradanicode@gmail.com>
2025-09-05 23:12:12 +0100

    complete image-upload  middleware

--- middlewares/upload-middleware.js
+++ middlewares/upload-middleware.js
@@ -8,9 +8,26 @@ const storage =multer.diskStorage({
     },
     filename:function(req,file,cb){
         cb(null,
-            file.fieldname + "-" +Date.now() + path.extname(file.originalname)
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
         )
     }
 })
 
 //file filter function
+const checkFileFilter = (req,file,cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null,true)
+    }else{
+        cb(new Error('Not an Image!Please upload only images'))
+    }
+}
+
+//multer middleware
+
+module.exports =multer({
+    storage:storage,
+    fileFilter:checkFileFilter,
+    limits:{
+        fileSize: 5*1024*1024 //5mb size limit
+    }
+})
