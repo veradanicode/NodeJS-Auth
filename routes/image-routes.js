ec0cd85f7ee689243555807c4d1e0460761dbe57
Vera D <veradanicode@gmail.com>
2025-09-05 23:12:55 +0100

    dd route to upload image

--- routes/image-routes.js
+++ routes/image-routes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const authMiddleware =require('../middlewares/auth-middleware')
-const adminMiddleware =require('../middlewares/admin-middleware')
-
+const adminMiddleware =require('../middlewares/admin-middleware');
+const uploadMiddleware = require('../middlewares/upload-middleware');
+const {uploadImageController} =require('../controllers/image-controllers')
 
 const router =express.Router();
 
 //upload the image
-router.post('/upload',authMiddleware,adminMiddleware )
+router.post(
+    '/upload',
+    authMiddleware,
+    adminMiddleware,
+    uploadMiddleware.single("image"),
+    uploadImageController
+)
 //get all images
 
 module.exports=router;
