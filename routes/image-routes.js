e649465990bdc8a2384b9c2fa0497851a93f3272
Vera D <veradanicode@gmail.com>
2025-09-05 20:33:38 +0100

    add image route

--- routes/image-routes.js
+++ routes/image-routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const authMiddleware =require('../middlewares/auth-middleware')
+const adminMiddleware =require('../middlewares/admin-middleware')
 
 
 const router =express.Router();
 
+//upload the image
+router.post('/upload',authMiddleware,adminMiddleware )
+//get all images
 
 module.exports=router;
