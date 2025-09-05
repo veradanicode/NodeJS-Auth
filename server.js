d801498fedc65b24025933bde972ad1f88930ded
Vera D <veradanicode@gmail.com>
2025-09-05 23:13:47 +0100

    add imaage upload in parent route

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const connectToDB = require('./database/db')
 const authRoutes =require('./routes/auth-routes')
 const homeRoutes =require('./routes/home-routes')
 const adminRoutes =require('./routes/admin-routes')
-
+const uploadImageRoute = require('./routes/image-routes')
 
 
 //connect to DB
@@ -20,6 +20,7 @@ app.use(express.json())
 app.use('/api/auth',authRoutes)
 app.use('/api/home',homeRoutes)
 app.use('/api/admin',adminRoutes)
+app.use('/api/image',uploadImageRoute)
 
 
 
