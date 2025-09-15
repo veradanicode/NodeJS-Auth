e2cfbc417d20379cefbe006fa36ef3e6e298d8fe
Vera D <veradanicode@gmail.com>
2025-09-15 22:08:37 +0100

    fix  google oauth route for google authentication

--- routes/google-auth-routes.js
+++ routes/google-auth-routes.js
@@ -7,13 +7,14 @@ router.get('/login',
 )
 
 router.get('/callback',
-     passport.authenticate('google'),{
+     passport.authenticate('google',{
             successRedirect:'/api/home/welcome',
             failureRedirect:'api/auth/google/failure'
-        }
+        })
 )
 
 router.get('api/auth/google/failure',(req,res)=>{
     res.status(401).send("Something went wrong!")
 })
+
 module.exports=router;
