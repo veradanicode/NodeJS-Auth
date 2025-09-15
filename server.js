0cca869f3e06ff2435caa2c375680b0df5c9695a
Vera D <veradanicode@gmail.com>
2025-09-15 21:49:04 +0100

    add google oauth parent route

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
-const express =require('express')
+require('dotenv').config();
+require('./config/oauth');
+const express =require('express');
 const connectToDB = require('./database/db')
 const authRoutes =require('./routes/auth-routes')
 const homeRoutes =require('./routes/home-routes')
 const adminRoutes =require('./routes/admin-routes')
 const uploadImageRoute = require('./routes/image-routes')
-
+const googleAuth=require('./routes/google-auth-routes')
 
 //connect to DB
 connectToDB()
@@ -21,8 +22,7 @@ app.use('/api/auth',authRoutes)
 app.use('/api/home',homeRoutes)
 app.use('/api/admin',adminRoutes)
 app.use('/api/image',uploadImageRoute)
-
-
+app.use('/api/auth/google',googleAuth)
 
 app.listen(PORT,()=>{
     console.log(`Server is now running at port ${PORT}`);
