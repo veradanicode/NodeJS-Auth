152bf17418e256adf5e3407def84ac9c4cfdfeb5
Vera D <veradanicode@gmail.com>
2025-10-01 15:09:44 +0100

    set app view engine and view route for testing

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const uploadImageRoute = require('./routes/image-routes')
 const googleAuth=require('./routes/google-auth-routes')
 const session =require('express-session');
 const passport = require('passport');
+
 //connect to DB
 connectToDB()
 
 const app = express();
 const PORT= process.env.PORT ||3000
+
+//set view engine
+app.set('view engine','ejs')
 
 //middlewares
 app.use(express.json())
@@ -24,12 +28,16 @@ app.use(session({
   cookie: { secure: true }
 }));app.use(passport.initialize())
 app.use(passport.session())
+
 //routes
 app.use('/api/auth',authRoutes)
 app.use('/api/home',homeRoutes)
 app.use('/api/admin',adminRoutes)
 app.use('/api/image',uploadImageRoute)
 app.use('/api/auth/google',googleAuth)
+app.use('/wow',(req,res)=>{
+  res.render('home')
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is now running at port ${PORT}`);
